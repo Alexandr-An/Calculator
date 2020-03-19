9aca59605cef60cc78d6678301b1ef626e3fd6a3
AlAndreev92 <brabusmax@mail.ru>
2020-03-19 12:51:05 +0300

    interface 2.0 (массив)

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -10,5 +10,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int[] numbersId = new int[]{
+                R.id.zero,
+                R.id.one,
+                R.id.two,
+                R.id.three,
+                R.id.four,
+                R.id.five,
+                R.id.six,
+                R.id.seven,
+                R.id.eight,
+                R.id.nine
+
+        };
+        int[] actionsId =  new int[]{
+          R.id.plus,
+          R.id.minus,
+          R.id.division,
+          R.id.multiplication,
+          R.id.equals
+        };
     }
 }

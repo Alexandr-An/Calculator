21964faa722fc5246d9df92bf7a63703334341c9
AlAndreev92 <brabusmax@mail.ru>
2020-03-19 16:21:06 +0300

    interface 2.0 (beta)

--- app/src/main/java/com/example/calculator/CalculatorModel.java
+++ app/src/main/java/com/example/calculator/CalculatorModel.java
@@ -8,6 +8,8 @@ public class CalculatorModel {
 
     private State state;
 
+    private int actionSelected;
+
     public CalculatorModel() {
         state = State.firstArgInput;
     }
@@ -17,11 +19,96 @@ public class CalculatorModel {
         secondArgInput,
         resultShow;
     }
-    public void onNumPressed(int buttonId){
+    public void onNumPressed(int buttonId){// реакция на нажатие кнопки с цифрой
 
+        if (state == State.resultShow){
+            state = State.firstArgInput;
+        }
+if (inputStr.length() < 10) {
+switch (buttonId){
+    case R.id.zero:
+        if (inputStr.length() != 0){
+            inputStr.append("0");
+        }break;
+    case R.id.one:
+            inputStr.append("1");
+        break;
+    case R.id.two:
+        inputStr.append("2");
+        break;
+    case R.id.three:
+        inputStr.append("3");
+        break;
+    case R.id.four:
+        inputStr.append("4");
+        break;
+    case R.id.five:
+        inputStr.append("5");
+        break;
+    case R.id.six:
+        inputStr.append("6");
+        break;
+    case R.id.seven:
+        inputStr.append("7");
+        break;
+    case R.id.eight:
+        inputStr.append("8");
+        break;case R.id.nine:
+        inputStr.append("9");
+        break;
+
+}
+}
     }
-    public void onActionPressed(int actionId){
+    public void onActionPressed(int actionId) {// реакция на кнопки арифметических действий и равно
 
+        if (actionId == R.id.equals && state == State.secondArgInput) {
+secondArg = Integer.parseInt(inputStr.toString());
+state = State.resultShow;
+inputStr.setLength(0);
+            inputStr.setLength(0);
+            switch (actionSelected) {
+                case R.id.plus:
+                    inputStr.append(firstArg + secondArg);
+                    break;
+                case R.id.minus:
+                    inputStr.append(firstArg - secondArg);
+                    break;
+                case R.id.multiplication:
+                    inputStr.append(firstArg * secondArg);
+                    break;
+                case R.id.division:
+                    inputStr.append(firstArg / secondArg);
+                    break;
+
+
+            }
+        } else if (inputStr.length() > 0 && state
+                == State.firstArgInput) {
+            firstArg = Integer.parseInt(inputStr.toString());
+        state = State.secondArgInput;
+        inputStr.setLength(0);
+        switch (actionId) {
+                case R.id.plus:
+                    actionSelected = R.id.plus;
+                    break;
+                case R.id.minus:
+                    actionSelected = R.id.minus;
+                    break;
+                case R.id.multiplication:
+                    actionSelected = R.id.multiplication;
+                    break;
+                case R.id.division:
+                    actionSelected = R.id.division;
+                    break;
+
+
+            }
+        }
+    }
+
+    public String getText(){
+        return inputStr.toString();
     }
 
 
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 private CalculatorModel calculator;
+private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,9 +32,36 @@ private CalculatorModel calculator;
           R.id.minus,
           R.id.division,
           R.id.multiplication,
-          R.id.equals
+                R.id.equals
+
+        };
+text = findViewById(R.id.text);
+        calculator = new CalculatorModel();
+
+        View.OnClickListener numberButtonClickListener =new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+calculator.onNumPressed(view.getId());
+
+text.setText(calculator.getText());
+            }
         };
 
-        calculator = new CalculatorModel();
+        View.OnClickListener actionButtonOneClickListener = new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                calculator.onActionPressed(view.getId());
+                text.setText(calculator.getText());
+
+            }
+        };
+        for (int i = 0; i < numbersId.length; i++) {
+findViewById(numbersId[i]).setOnClickListener(numberButtonClickListener);
+        }
+        for (int i = 0; i < actionsId.length ; i++) {
+            findViewById(actionsId[i]).setOnClickListener(actionButtonOneClickListener);
+        }
     }
 }
